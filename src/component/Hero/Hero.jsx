7cd2fed17b0412f73de60e37390e4c81c0d87bad
emonh0que <you@example.com>
2023-04-03 07:52:51 +0600

    created the figure div

--- src/component/Hero/Hero.jsx
+++ src/component/Hero/Hero.jsx
@@ -16,12 +16,37 @@ const Hero = () => {
                 {/* Hero Heading  */}
                 <div className="hero-text">
                     <div>
-                        <span>Shape</span>
+                        <span className='stroke -text'>Shape</span>
                         <span> Your</span>
                     </div>
                     <div>
                         <span>Ideal body</span>
                     </div>
+                    <div>
+                        <span>
+                        In here we will help you to shape and build your ideal body and live up your life to fullest
+                        </span>
+                    </div>
+                </div>
+
+                {/* figures  */}
+                <div className="figures">
+                    <div>
+                        <span>140+</span>
+                        <span>expert coaches</span>
+                    
+                    </div>
+                    <div>
+                        <span>+978</span>
+                        <span>members joined</span>
+                    
+                    </div>
+                    <div>
+                        <span>50+</span>
+                        <span>fitness programm</span>
+                    
+                    </div>
+                    
                 </div>
             </div>
             <div className="right-h">right side</div>
