3752023627ee914830dbe1bd09303d24acebbeac
emonh0que <you@example.com>
2023-04-03 09:20:52 +0600

    started woking on the reasons section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Hero from './component/Hero/Hero';
 import Programs from './component/Programs/Programs';
+import Reasons from './component/Reasons/Reasons';
 function App() {
   return (
     <div className="App">
           <Hero></Hero>
           <Programs></Programs>
+          <Reasons></Reasons>
     </div>
   );
 }
