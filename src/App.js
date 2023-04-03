6ffc01b94a011547b02008c509533ca6d1ae64ce
emonh0que <you@example.com>
2023-04-03 09:12:23 +0600

    doing great with category section

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import Hero from './component/Hero/Hero';
+import Programs from './component/Programs/Programs';
 function App() {
   return (
     <div className="App">
           <Hero></Hero>
+          <Programs></Programs>
     </div>
   );
 }
