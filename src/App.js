53d531cdb98d2816821fe3720ac6c69ab6a474ce
emonh0que <you@example.com>
2023-04-06 17:20:43 +0600

    successfully completed footer

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import Footer from './component/Footer/Footer';
 import Hero from './component/Hero/Hero';
 import Join from './component/Join/Join';
 import Plans from './component/Plans/Plans';
@@ -14,6 +15,7 @@ function App() {
           <Plans></Plans>
           <Testimonial></Testimonial>
           <Join></Join>
+          <Footer></Footer>
     </div>
   );
 }
