586a6989dde5d6115fa5c1811bda507d1fda1fcf
emonh0que <you@example.com>
2023-04-06 17:12:04 +0600

    added footer

---
+++ src/component/Footer/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Footer.css'
+import Github from '../../assets/github.png'
+import Instagram from '../../assets/instagram.png'
+import Linkedin from '../../assets/linkedin.png'
+import Logo from '../../assets/logo.png'
+const Footer = () => {
+    return (
+        <div className='footer-container'>
+            
+        </div>
+    );
+};
+
+export default Footer;
